a6994077c39a30654746079161622c9fe9207409
Hukam <115365309+rathorehukam@users.noreply.github.com>
2023-10-23 12:32:59 +0530

    Create Imagine you are building a software application for a prestigious school.cpp

---
+++ Unit 5/L - 33/Imagine you are building a software application for a prestigious school.cpp
@@ -0,0 +1,59 @@
+
+
+
+
+#include <iostream>
+using namespace std;
+
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void buildMaxHeap(int arr[], int n) {
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+}
+
+void deleteMax(int arr[], int& n) {
+    arr[0] = arr[n - 1];
+    n--;
+    heapify(arr, n, 0);
+}
+
+void displayHeap(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    buildMaxHeap(arr, n);
+
+    deleteMax(arr, n);
+
+    displayHeap(arr, n);
+
+    return 0;
+}
