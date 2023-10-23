74c0913f50ac5f9a0f99c84a3a8492b64d9f38c0
Hukam <115365309+rathorehukam@users.noreply.github.com>
2023-10-24 00:48:28 +0530

    Create As a developer in an IT company schedule tasks depending on their time consumption, arranging them in descending order from highest to lowest time consumption.cpp

---
+++ Unit 5/L - 32/As a developer in an IT company schedule tasks depending on their time consumption, arranging them in descending order from highest to lowest time consumption.cpp
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+void MaxHeapify(int arr[], int i, int N)
+{
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    int largest = i;
+
+    if (l < N && arr[l] > arr[i])
+        largest = l;
+    if (r < N && arr[r] > arr[largest])
+        largest = r;
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        MaxHeapify(arr, largest, N);
+    }
+}
+
+void convertMaxHeap(int arr[], int N)
+{
+    for (int i = (N - 2) / 2; i >= 0; --i)
+        MaxHeapify(arr, i, N);
+}
+
+
+void printArray(int* arr, int size)
+{
+    for (int i = 0; i < size; ++i)
+        cout << arr[i] << " ";
+    cout<<endl;
+}
+
+int main()
+{
+    int arrayCount;
+    cin>>arrayCount;
+    int arr[arrayCount];
+    for (int i=0;i<arrayCount;i++){
+        cin>>arr[i];
+    }
+
+    convertMaxHeap(arr, arrayCount);
+
+    printArray(arr, arrayCount);
+    cout<<arr[0]+arr[arrayCount-1]<<endl;
+
+    return 0;
+}
