06049c8ab96a0c485d25b4a646d0ae5c67ca396f
Hukam <115365309+rathorehukam@users.noreply.github.com>
2023-10-27 22:48:52 +0530

    Update and rename Write a program to create a binary search tree (BST) from user input integers. After constructing the BST, use a preorder traversal approach to print only the values found at odd depths within the tree. cpp to Write a program to create a binary search tree (BST) from user input integers. After constructing the BST, use a preorder traversal approach to print only the values found at odd depths within the tree.cpp

--- Unit 5/L - 34/Write a program to create a binary search tree (BST) from user input integers. After constructing the BST, use a preorder traversal approach to print only the values found at odd depths within the tree.cpp
+++ Unit 5/L - 34/Write a program to create a binary search tree (BST) from user input integers. After constructing the BST, use a preorder traversal approach to print only the values found at odd depths within the tree.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 
-// Define the structure for a BST node
 struct Node {
     int data;
     Node* left;
     Node* right;
 };
-
-// Function to create a new node
 Node* createNode(int data) {
     Node* newNode = new Node;
     newNode->data = data;
@@ -16,7 +13,6 @@ Node* createNode(int data) {
     return newNode;
 }
 
-// Function to insert a node into the BST
 Node* insert(Node* root, int data) {
     if (root == nullptr) {
         return createNode(data);
@@ -44,17 +40,13 @@ void printOddDepthValues(Node* root, int depth) {
 int main() {
     Node* root = nullptr;
     int n, data;
-
-    // Input the number of elements
     std::cin >> n;
 
-    // Input elements and insert them into the BST
     for (int i = 0; i < n; i++) {
         std::cin >> data;
         root = insert(root, data);
     }
 
-    // Print values at odd depths using preorder traversal
     printOddDepthValues(root, 1);
     std::cout << std::endl;
 
