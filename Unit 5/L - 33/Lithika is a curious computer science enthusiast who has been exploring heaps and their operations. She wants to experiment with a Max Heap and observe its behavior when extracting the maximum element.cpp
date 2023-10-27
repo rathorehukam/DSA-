093f38c17d67366d58596e1eb5b1484ab2f1bed8
Hukam <115365309+rathorehukam@users.noreply.github.com>
2023-10-27 15:19:33 +0530

    Update Lithika is a curious computer science enthusiast who has been exploring heaps and their operations. She wants to experiment with a Max Heap and observe its behavior when extracting the maximum element.cpp

--- Unit 5/L - 33/Lithika is a curious computer science enthusiast who has been exploring heaps and their operations. She wants to experiment with a Max Heap and observe its behavior when extracting the maximum element.cpp
+++ Unit 5/L - 33/Lithika is a curious computer science enthusiast who has been exploring heaps and their operations. She wants to experiment with a Max Heap and observe its behavior when extracting the maximum element.cpp
@@ -7,65 +7,74 @@
 
 
 
+
+
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-#define MAX_SIZE  100; 
-
-void heapify(int arr[], int n, int i) {
+void maxHeapify(vector<int>& arr, int n, int i) {
     int largest = i;
-    int l = 2 * i + 1;
-    int r = 2 * i + 2;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
 
-    if (l < n && arr[l] > arr[largest])
-        largest = l;
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
 
-    if (r < n && arr[r] > arr[largest])
-        largest = r;
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
 
     if (largest != i) {
         swap(arr[i], arr[largest]);
-        heapify(arr, n, largest);
+        maxHeapify(arr, n, largest);
     }
 }
 
-void buildMaxHeap(int arr[], int n) {
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i);
+void buildMaxHeap(vector<int>& arr) {
+    int n = arr.size();
+
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        maxHeapify(arr, n, i);
+    }
 }
 
-int extractMax(int arr[], int& n) {
+int extractMax(vector<int>& arr) {
+    int n = arr.size();
+    if (n <= 0) {
+        return -1;
+    }
 
     int maxElement = arr[0];
-    arr[0] = arr[n - 1];
-    n--;
-    heapify(arr, n, 0);
+    swap(arr[0], arr[n - 1]);
+    arr.pop_back();
+    maxHeapify(arr, n - 1, 0);
 
     return maxElement;
-}
-
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; ++i)
-        cout << arr[i] << " ";
 }
 
 int main() {
     int n;
     cin >> n;
+    vector<int> arr(n);
 
-
-    int arr[MAX_SIZE];
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
-
-    buildMaxHeap(arr, n);
-
-    int maxElement = extractMax(arr, n);
-    if (maxElement != -1) {
-        cout << maxElement << endl;
     }
 
-    printArray(arr, n);
+    buildMaxHeap(arr);
+    int maxElement = extractMax(arr);
+
+    cout << maxElement << endl;
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i];
+        if (i != arr.size() - 1) {
+            cout << " ";
+        }
+    }
+    cout << endl;
 
     return 0;
 }
